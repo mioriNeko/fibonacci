8e54e07662240d06e5f11f273449235edf1963d2
midori.n <uruoilemon-techx@yahoo.co.jp>
2020-03-28 17:29:41 +0900

    フィボナッチ関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+// このままだと恐ろしく時間がかかってしまう処理。アルゴリズムの改善を試みる
+function fib(n) {
+  if (n === 0) {
+    return 0;
+  }else if(n === 1){
+    return 1;
+  }
+  return fib(n - 2) + fib(n - 1);
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++){
+  console.log(fib(i));
+}
